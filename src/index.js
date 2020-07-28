bbd46cba01ac9f7f90a3004993528a2d30d54a6f
Jeffrey Cheng <chengj23@uw.edu>
2020-07-28 03:08:40 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class HiddenInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true,
+    };
+  }
+  render() {
+    let button;
+    if (this.state.hidden) {
+
+    } else {
+      button = <p> This application is still a work in progress </p>;
+    };
+    return (
+      <div class="hiddenInfo">
+        <h2>Hidden Information</h2>
+        <button class="hideButton" onClick={() => this.setState({
+          hidden: !this.state.hidden,
+        })}>
+          show/hide
+        </button>
+        {button}
+      </div>
+    )
+  }
+}
+
+class MainPage extends 	React.Component {
+  render() {
+    return (
+      <div class="main">
+        <div class="navbar">
+          <a href="#">About</a>
+          <div class="dropdown">
+            <button class="dropbtn">
+              Files
+            </button>
+            <div class="dropdown-content">
+              <a href="#">Resume</a>
+              <a href="#">Transcript</a>
+            </div>
+          </div>
+          <a href="https://www.linkedin.com/in/jeffrey-cheng23/">LinkedIn</a>
+          <a href="https://github.com/jeffrey8759">GitHub</a>
+          <a href="#">Contact</a>
+        </div>
+        <div class='mainFrame'> 
+          <h1> Jeffrey Cheng </h1>
+          <img id='profile_pic' src='https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/106243546_3354141631302843_4473972968588926169_n.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=nRIdlhMQhZAAX-jNyOo&_nc_ht=scontent-sea1-1.xx&oh=2fe648b2eb90bdd87b04f241654e5ada&oe=5F434002'/>
+          <HiddenInfo />
+        </div>
+      </div>
+    )
+	}
+}
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<MainPage />,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
